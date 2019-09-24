5d663415703017a60bf6f7c781c30a6238f4e14a
803146 <54371457+803146@users.noreply.github.com>
2019-09-24 10:30:28 -0700

    Add files via upload

---
+++ ScannerTest/Person.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Person here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(String fullName)
+    {
+       String fName,mName,lName;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String parseName(String fullName)
+    {
+        int test;
+        
+        int Si = fullName.indexOf(", ");
+        lName = fullName.substring(0,Si);
+        return fullName;
+    }
+}
