3b47e9bd7b739f663ff7e6cf9b49c3f09c6ebf65
803146 <54371457+803146@users.noreply.github.com>
2019-10-17 10:30:03 -0700

    kjdhasfljdhasfkl

---
+++ MeanMedianMode/MMM.java
@@ -0,0 +1,31 @@
+package MeanMedianMode;
+
+
+/**
+ * Write a description of class MMM here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class MMM
+{
+    // instance variables 
+    public void printArray(){}
+    public int getSum(){return 0;}
+    public double getMean(){return 0.0;}
+    public double getMedian(){return 0.0;}
+    ArrayList<Integar> array = new ArrayList<Integar>
+    /**
+     * Constructor for objects of class MMM
+     */
+    public MMM()
+    {
+
+    }
+    public void loadArray(){}
+    
+
+        
+    }
+    
---
+++ MeanMedianMode/MMM_Runner.java
@@ -0,0 +1,16 @@
+package MeanMedianMode;
+
+
+/**
+ * Write a description of class MMM_Runner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MMM_Runner
+{
+public static void main(){
+    MMM meanMedianMode = new MMM();
+    
+}
+}
